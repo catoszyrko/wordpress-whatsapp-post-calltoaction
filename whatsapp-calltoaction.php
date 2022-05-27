c093be80e2a7a046745c15cabb01baa532ce8b09
Yamil <yamil@Yamils-MacBook-Air.local>
2022-05-27 11:34:32 -0300

    fontawesome added

--- whatsapp-calltoaction.php
+++ whatsapp-calltoaction.php
@@ -6,6 +6,13 @@
  * Author: Yamil Szyrko
  */
 
+add_action('wp_enqueue_scripts', 'loadfontawesomecss', 999);
+
+function loadfontawesomecss(){
+	wp_register_style( 'fontawesomecss', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css");
+	wp_enqueue_style( 'fontawesomecss');
+}
+
 add_shortcode( "wh-ca-btn", function($atts, $content){
 	$styles = "
 			display:block;
@@ -16,13 +23,14 @@ add_shortcode( "wh-ca-btn", function($atts, $content){
             color:white;
             box-shadow:1px 1px 1px #333333;
 			text-align:center;
+			border-radius:10px;
         ";
 	
 	$phone = str_replace("+","",$atts['phone']);
 	$cta = $atts['cta'];
 
 	return '<div class="responsiveContent">
-				<a target="blank" href="https://api.whatsapp.com/send?phone='.$phone.'" style="'.$styles.'">'.$cta.'</a>
+				<a target="blank" href="https://api.whatsapp.com/send?phone='.$phone.'" style="'.$styles.'"> <i class="fa fa-phone"></i> '.$cta.'</a>
 			</div>';
 });
 ?>
